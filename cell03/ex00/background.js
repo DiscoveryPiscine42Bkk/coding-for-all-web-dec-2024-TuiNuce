0b2108cc62fda23922b0ad6f33878de49fe87438
TuiNuce <nattaphat2248@gmail.com>
2024-12-19 21:24:28 +0700

    Update background.js

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -1,8 +1,8 @@
 function getRandomColor() {
-  const r = Math.floor(Math.random() * 256); // สุ่มค่าแดง (0-255)
-  const g = Math.floor(Math.random() * 256); // สุ่มค่าสีเขียว (0-255)
-  const b = Math.floor(Math.random() * 256); // สุ่มค่าสีน้ำเงิน (0-255)
-  return `rgb(${r}, ${g}, ${b})`; // คืนค่ารูปแบบ RGB
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 const button = document.getElementById("changeColorButton");
